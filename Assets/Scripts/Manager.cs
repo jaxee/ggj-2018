1a6067388859e7067db282f0c7e571a36358de95
Andrew <ajsmith22@live.com>
2018-01-26 20:43:30 -0500

    merge

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	public Room[] rooms;
+	public int numberOfPeople;
+	public int difficulty;
+	private int numberOfDoors = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < rooms.Length; i++) {
+			numberOfDoors += rooms[i].getNumberOfDoors();
+		}
+
+		Debug.Log ("Number of Doors: " + numberOfDoors);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	public int typeOfRoom; // 1 is normal, 2 is hazard, 3 is escape
+	public int numberOfDoors;
+	private int numberOfSick;
+	private int numberOfHealthy;
+	private bool[] doorStatus;
+	private List<GameObject> Transform = new List<GameObject>();
+
+	public Room (int type, int healthy, int sick) {
+		typeOfRoom = type;
+		numberOfHealthy = healthy;
+		numberOfSick = sick;
+
+		createRoom ();
+		createPeople ();
+	}
+	
+	void Update () {
+		// Keep track of the number of healthy and sick people in a room
+	}
+
+	private void createRoom() {
+
+		for (int i = 0; i < numberOfDoors; i++) {
+			doorStatus [i] = true;
+		}
+
+		if (typeOfRoom == 1) {
+			// Create a normal room
+		} else if (typeOfRoom == 2) {
+			// Create an hazard room
+		} else {
+			// Create an escape room
+		}
+	}
+
+	private void createPeople() {
+		for (int i = 0; i < numberOfHealthy; i++) {
+			// Create healthy people
+		}
+
+		for (int j = 0; j < numberOfSick; j++) {
+			// Create sick people
+		}
+	}
+
+	public int getNumberOfDoors () {
+		return numberOfDoors;
+	}
+
+	public int getNumberOfPeople () {
+		return numberOfHealthy + numberOfSick;
+	}
+}
