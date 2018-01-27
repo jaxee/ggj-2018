3fdfff1afde06e5dde4d4f5cb9323a816d4e5e22
Jackie Ellis <jackie.ellis@hotmail.com>
2018-01-26 19:46:15 -0500

    Model for Manager and Room

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	public Room[] rooms;
+	public int numberOfRooms;
+	private int numberOfDoors;
+	private bool[] allDoors;
+
+	// Use this for initialization
+	void Start () {
+
+		rooms = GameObject.FindGameObjectsWithTag("Room");
+
+		for (int i = 0; i < numberOfRooms; i++) {
+			numberOfDoors += rooms[i].getNumberOfDoors ();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
