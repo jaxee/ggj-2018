3de24958668c1bed76197e07581424c2596c260c
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-01-27 19:20:38 -0500

    Scanning animation/doors

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -15,6 +15,16 @@ public class Interact : MonoBehaviour {
 	bool isAirLock;
 	bool isScanner;
 
+	//You'll float too
+	float airlockCD = 15f;
+	float airlockTimer;
+	bool airlockOpen = false;
+
+	float scannerCD = 30f;
+	bool canScan = true;
+
+	bool interactable = true;
+
 	// Escape pod variables
 	bool isEscapePod;
 	bool isEscapePodBoarding;
@@ -33,34 +43,40 @@ public class Interact : MonoBehaviour {
 			isDoor = true;
 		} else if (tag == "Escape") {
 			isEscapePod = true;
-		}
+		} else if (tag == "Airlock")
+			isAirLock = true;
+		else
+			isScanner = true;
 		oldColor = meshRender.material.color;
 	}
 
 	void OnMouseEnter(){
-		meshRender.enabled = true;
-		if (Manager.doors.Count == manager.maxDoorsClosed) {
-			Manager.doors [0].meshRender.enabled = true;
-			Manager.doors [0].meshRender.material.color = new Color (1f, 0f, 0f, 0.5f);
+		if (interactable) {
+			meshRender.enabled = true;
+			if (Manager.doors.Count == manager.maxDoorsClosed && isDoor) {
+				Manager.doors [0].meshRender.enabled = true;
+				Manager.doors [0].meshRender.material.color = new Color (1f, 0f, 0f, 0.5f);
+			}
 		}
 	}
 
 	void OnMouseExit(){
 		meshRender.enabled = false;
-		if (Manager.doors.Count >= manager.maxDoorsClosed) {
+		if (Manager.doors.Count >= manager.maxDoorsClosed && isDoor) {
 			Manager.doors [0].meshRender.enabled = false;
 			Manager.doors [0].meshRender.material.color = oldColor;
 		}
 	}
 
 	void OnMouseDown(){
-		Trigger ();
+		if(interactable)
+			Trigger ();
 	}
 
 	public void Trigger(){
-		if(meshObs != null)
+		if (meshObs != null)
 			meshObs.enabled = !meshObs.enabled;
-		
+
 		anim.SetTrigger ("toggle");
 
 		if (isDoor) {
@@ -69,16 +85,16 @@ public class Interact : MonoBehaviour {
 			} else {
 				Manager.doors.Remove (this);
 			}
-			if (Manager.doors.Count >= manager.maxDoorsClosed) {
+			if (Manager.doors.Count >= manager.maxDoorsClosed && isDoor) {
 				Manager.doors [0].meshRender.enabled = false;
 				Manager.doors [0].meshRender.material.color = oldColor;
 			}
 		}
-			
+		
 		if (isEscapePod) {
 			isEscapePodBoarding = true;
 
-			currentRoom = transform.parent.GetComponent<Room>();
+			currentRoom = transform.parent.GetComponent<Room> ();
 			foreach (GameObject door in currentRoom.doors) {
 				Interact d = door.GetComponent<Interact> ();
 				if (!d.meshObs.enabled) {
@@ -91,11 +107,11 @@ public class Interact : MonoBehaviour {
 			if (currentRoom.playersInRoom.Count == 0) {
 				Debug.Log ("LAUNCH SHIP ANIMATION");
 			} else {
-				foreach(GameObject player in currentRoom.playersInRoom) {
+				foreach (GameObject player in currentRoom.playersInRoom) {
 					AIComponent p = player.GetComponent<AIComponent> ();
-					int r = escapePodEntrance.Next(evacuationPoints.Count);
-					Debug.Log ("Go to entrance: " + evacuationPoints[r]);
-					p.meshAgent.SetDestination (evacuationPoints[r].transform.position);
+					int r = escapePodEntrance.Next (evacuationPoints.Count);
+					Debug.Log ("Go to entrance: " + evacuationPoints [r]);
+					p.meshAgent.SetDestination (evacuationPoints [r].transform.position);
 					p.meshAgent.isStopped = false;
 					p.meshAgent.speed = 9f;
 					p.isBoarding = true;
@@ -104,6 +120,30 @@ public class Interact : MonoBehaviour {
 			}
 		}
 
+		if (isAirLock) {
+			StartCoroutine (DoAirlockStuff ());
+		}
+
+		if (isScanner && canScan) {
+			StartCoroutine (DoScannerStuff());
+			canScan = false;
+		}
+
+
+	}
+
+	void Update(){
+		//If the airlock is currently open, the cooldown has elapsed, and the room is empty, reset.
+		if (airlockOpen && Time.time - airlockTimer > airlockCD && transform.root.GetComponent<Room>().playersInRoom.Count == 0) {
+			anim.SetTrigger ("toggle");
+			airlockOpen = false;
+			foreach (GameObject door in currentRoom.doors) {
+				Interact d = door.GetComponent<Interact> ();
+				d.interactable = true;
+				d.anim.SetTrigger ("toggle");
+				d.meshObs.enabled = false;
+			}
+		}
 	}
 
 	public void OnTriggerEnter(Collider c){
@@ -116,5 +156,57 @@ public class Interact : MonoBehaviour {
 				Debug.Log ("LAUNCH SHIP ANIMATION");
 			}
 		}
+	}
+
+	IEnumerator DoAirlockStuff(){
+		yield return new WaitForSeconds (1f);
+		airlockTimer = Time.time;
+		airlockOpen = true;
+		SealDoors ();
+		transform.root.GetComponentInChildren<ParticleSystem> ().Play ();
+		foreach (GameObject player in currentRoom.playersInRoom) {
+			AIComponent p = player.GetComponent<AIComponent> ();
+			Destroy (p.meshAgent);
+			p.pull = true;
+			Rigidbody rb = p.GetComponent<Rigidbody> ();
+			rb.constraints = RigidbodyConstraints.None;
+			rb.useGravity = false;
+			rb.drag = 0f;
+			rb.angularDrag = 0f;
+		}
+	}
+
+	void SealDoors(){
+		currentRoom = transform.root.GetComponent<Room> ();
+		foreach (GameObject door in currentRoom.doors) {
+			Interact d = door.GetComponent<Interact> ();
+			if (!d.meshObs.enabled) {
+				d.anim.SetTrigger ("toggle");
+				d.meshObs.enabled = true;
+				d.interactable = false;
+			}
+		}
+	}
+
+	IEnumerator DoScannerStuff(){
+		SealDoors ();
+		yield return new WaitForSeconds (1.5f);
+		Debug.Log ("Scan!");
+		StartCoroutine (UnlockDoors ());
+		StartCoroutine (ScannerCooldown ());
+	}
+
+	IEnumerator UnlockDoors(){
+		yield return new WaitForSeconds (1.5f);
+		foreach (GameObject door in currentRoom.doors) {
+			Interact d = door.GetComponent<Interact> ();
+			d.interactable = true;
+		}
+	}
+
+	IEnumerator ScannerCooldown(){
+		yield return new WaitForSeconds (scannerCD);
+		canScan = true;
+		anim.SetTrigger ("toggle");
 	}
 }
