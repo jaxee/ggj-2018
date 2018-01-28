e4a00f5d4e5e74e3ab813a94fe67d393700a21da
Raiquon Welcome <raiquonwelcome@cmail.carleton.ca>
2018-01-27 23:31:16 -0500

    David stuff

---
+++ Assets/Scripts/PersonPick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonPick : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject[] people;
+
+	// Use this for initialization
+	void Start () {
+		int choice;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
