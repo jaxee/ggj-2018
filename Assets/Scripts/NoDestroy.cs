6b12db428a13699b969de4cab3ca9fd71731c9df
Raiquon Welcome <raiquonwelcome@cmail.carleton.ca>
2018-01-28 12:11:21 -0500

    no music repeat

--- Assets/Scripts/NoDestroy.cs
+++ Assets/Scripts/NoDestroy.cs
@@ -5,4 +5,12 @@ public class NoDestroy : MonoBehaviour {
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
 	}
+
+
+	void Start(){
+			if (GameObject.FindGameObjectsWithTag ("Music").Length > 1) {
+			Destroy (gameObject);
+		}
+	}
+
 }
